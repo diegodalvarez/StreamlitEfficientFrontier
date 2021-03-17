a4686f2ec4851d6e7ee8681770de688d5baef368
diegodalvarez <48641554+diegodalvarez@users.noreply.github.com>
2021-03-17 12:24:17 -0600

    Update efficient_frontier_streamlit_base.py
    separate class file, add in purpose and disclaimer

--- efficient_frontier_streamlit_base.py
+++ efficient_frontier_streamlit_base.py
@@ -1,119 +1,25 @@
 import pandas as pd
+import altair as alt
 import yfinance as yf
 import datetime as dt
 import streamlit as st
+import pandas_datareader as web
 import matplotlib.pyplot as plt
 
-import numpy as np
-import pandas as pd  
-import datetime as dt
-import streamlit as st
-
-class Efficient_Frontier:
-    
-    def __init__(self, stock_df, tickers):
-        
-        #what is inputted
-        self.stock_df = stock_df
-        self.tickers = tickers
-    
-    '''    
-    def returns_calculation(self):
-
-        returns_method_list = ['historical_average', "bullish", "bearish", "statistics", "LSTM"]
-
-        if self.returns_method == "historical_average":
-
-            returns = self.stock_df.pct_change().mean()
-            return returns
-
-        if self.returns_method == "bullish":
-
-            print("still working on bullish")
-
-        if self.returns_method == "bearish":
-
-            print("still working on bearish")
-
-        if self.returns_method == "statistics":
-
-            print("still working on statistics")
-
-        if self.returns_method == "LSTM":
-
-            print("still working on LSTM")
-
-        else:
-            print("incorrect returns method, use any below:")
-            for i in returns_method_list:
-                print(i)
-
-    def risk_calculation(self):
-
-        risk_measure_list = ["covaraince"]
-
-        if self.risk_measure == "covariance":
-
-            risk = self.stock_df.pct_change().cov()
-
-            return risk
-        else:
-
-            print("incorret risk method, use any below")
-            for i in risk_measure_list:
-                print(i)
-    '''
-    
-    def calc_portfolio_perf(self, weights, mean_returns, cov, rf):
-        
-        portfolio_return = np.sum(mean_returns * weights) * 252
-        portfolio_std = np.sqrt(np.dot(weights.T, np.dot(cov, weights))) * np.sqrt(252)
-        sharpe_ratio = (portfolio_return - rf) / portfolio_std
-        
-        return portfolio_return, portfolio_std, sharpe_ratio
-
-    def simulate_random_portfolios(self, num_portfolios, mean_returns, cov, rf, tickers):
-        
-        results_matrix = np.zeros((len(mean_returns)+3, num_portfolios))
-        
-        for i in range(num_portfolios):
-            
-            if i % 10000 == 0:
-                
-                st.write("simulated", '{:,}'.format(i) , "portfolios") 
-            
-            
-            weights = np.random.random(len(mean_returns))
-            weights /= np.sum(weights)
-            
-            portfolio_return, portfolio_std, sharpe_ratio = self.calc_portfolio_perf(weights, mean_returns, cov, rf)
-            
-            results_matrix[0,i] = portfolio_return
-            results_matrix[1,i] = portfolio_std
-            results_matrix[2,i] = sharpe_ratio
-            
-            for j in range(len(weights)):
-                
-                results_matrix[j+3,i] = weights[j]
-        
-        tickers = tickers.split(",")
-        results_df = pd.DataFrame(results_matrix.T,columns=['ret','stdev','sharpe'] + [ticker for ticker in tickers])
-        
-        return results_df  
-
-        
-    def find_portfolios(self, results):
-        
-        max_sharpe_port = results.iloc[results['sharpe'].idxmax()]
-        min_vol_port = results.iloc[results['stdev'].idxmin()]
-        
-        return max_sharpe_port, min_vol_port
+from efficient_frontier import *
 
 returns_methods = ["means_returns"]
 risk_methods = ["covariance"]
 
 st.header("Efficient Frontier")
-tickers = st.text_input('Please enter tickers here (seperated by comma):')
+with st.beta_expander('purpose'):
+    st.write("One of my interests is in portfolio management and optimization, so I've built in a \
+             framework for building efficient frontiers, and plan to add more. Such as rebalancing \
+                 multiple returns and risk methods, and optimization techniques. Some of the tools in \
+                     this application will be backed by research and experimentation on my github")
+                     
+tickers = st.text_input("Please enter tickers here (seperate):")
+st.text("ex for Microsoft, Apple, Amazon, Google, Facebook would be 'MSFT, AAPL, AMZN, GOOG, FB'")
 status_radio = st.radio('Please click Search when you are ready.', ('Entry', 'Search'))
 
 today = dt.date.today()
@@ -171,6 +77,8 @@ if find_frontier == True:
         st.write("maximum sharpe portfolio", portfolios[0])
         st.write("mininum variance portfolio", portfolios[1]) 
 
-    
+st.write('Disclaimer: Information and output provided on this site does \
+         not constitute investment advice.')
+st.write('Created by Diego Alvarez')
     
     
