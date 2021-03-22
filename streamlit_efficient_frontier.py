4b1bdb11bf7d18d78cd3b9ebbecfb4114d33b219
diegodalvarez <48641554+diegodalvarez@users.noreply.github.com>
2021-03-21 19:54:59 -0600

    update returns and risk

--- streamlit_efficient_frontier.py
+++ streamlit_efficient_frontier.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd  
 import datetime as dt
 import streamlit as st
+import pandas_datareader as web
 
 class Efficient_Frontier:
     
@@ -11,52 +12,19 @@ class Efficient_Frontier:
         self.stock_df = stock_df
         self.tickers = tickers
     
-    '''    
-    def returns_calculation(self):
-
-        returns_method_list = ['historical_average', "bullish", "bearish", "statistics", "LSTM"]
-
-        if self.returns_method == "historical_average":
-
+    def returns_calculation(self, method):
+        
+        if method == "mean_returns":
+            
             returns = self.stock_df.pct_change().mean()
             return returns
-
-        if self.returns_method == "bullish":
-
-            print("still working on bullish")
-
-        if self.returns_method == "bearish":
-
-            print("still working on bearish")
-
-        if self.returns_method == "statistics":
-
-            print("still working on statistics")
-
-        if self.returns_method == "LSTM":
-
-            print("still working on LSTM")
-
-        else:
-            print("incorrect returns method, use any below:")
-            for i in returns_method_list:
-                print(i)
-
-    def risk_calculation(self):
-
-        risk_measure_list = ["covaraince"]
-
-        if self.risk_measure == "covariance":
-
+        
+    def risk_calculation(self, method):
+        
+        if method == "covariance" or method == "cov":
+            
             risk = self.stock_df.pct_change().cov()
-
             return risk
-        else:
-
-            print("incorret risk method, use any below")
-            for i in risk_measure_list:
-                print(i)
-    '''
     
     def calc_portfolio_perf(self, weights, mean_returns, cov, rf):
         
@@ -66,31 +34,32 @@ class Efficient_Frontier:
         
         return portfolio_return, portfolio_std, sharpe_ratio
 
-    def simulate_random_portfolios(self, num_portfolios, mean_returns, cov, rf, tickers):
+    def simulate_random_portfolios(self, returns_method, risk_method, num_portfolios, rf, tickers):
         
-        results_matrix = np.zeros((len(mean_returns)+3, num_portfolios))
+        returns = self.returns_calculation(returns_method)
+        risk = self.risk_calculation(risk_method)
+
+        results_matrix = np.zeros((len(returns)+3, num_portfolios))
         
         for i in range(num_portfolios):
             
             if i % 10000 == 0:
-                
+        
                 st.write("simulated", '{:,}'.format(i) , "portfolios") 
             
-            
-            weights = np.random.random(len(mean_returns))
+            weights = np.random.random(len(returns))
             weights /= np.sum(weights)
             
-            portfolio_return, portfolio_std, sharpe_ratio = self.calc_portfolio_perf(weights, mean_returns, cov, rf)
+            portfolio_return, portfolio_std, sharpe_ratio = self.calc_portfolio_perf(weights, returns, risk, rf)
             
             results_matrix[0,i] = portfolio_return
             results_matrix[1,i] = portfolio_std
             results_matrix[2,i] = sharpe_ratio
             
             for j in range(len(weights)):
+                results_matrix[j+3,i] = weights[j]       
                 
-                results_matrix[j+3,i] = weights[j]
-        
-        tickers = tickers.split(",")
+        tickers = tickers.split(',')
         results_df = pd.DataFrame(results_matrix.T,columns=['ret','stdev','sharpe'] + [ticker for ticker in tickers])
         
         return results_df  
